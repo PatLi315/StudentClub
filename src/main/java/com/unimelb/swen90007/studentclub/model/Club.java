2abcabe3671aac730dd4284611e9527030483bef
PatLi315 <zeqianl1@student.unimelb.edu.au>
2024-09-01 23:21:23 +1000

    Adding club files

---
+++ src/main/java/com/unimelb/swen90007/studentclub/model/Club.java
@@ -0,0 +1,34 @@
+package com.unimelb.swen90007.studentclub.model;
+
+public class Club {
+    private int id;
+    private String name;
+
+    // Constructor
+    public Club(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Constructor without ID (for inserting new clubs)
+    public Club(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
