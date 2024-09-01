43540de6161684e05d845960fef6516710a7d513
PatLi315 <zeqianl1@student.unimelb.edu.au>
2024-09-01 23:22:12 +1000

    RSVP function bug fixing

--- src/main/java/com/unimelb/swen90007/studentclub/dao/RSVPDAO.java
+++ src/main/java/com/unimelb/swen90007/studentclub/dao/RSVPDAO.java
@@ -1,5 +1,6 @@
 package com.unimelb.swen90007.studentclub.dao;
 
+import com.unimelb.swen90007.studentclub.model.RSVP;
 import com.unimelb.swen90007.studentclub.util.DatabaseConnection;
 
 import java.sql.Connection;
@@ -11,22 +12,22 @@ public class RSVPDAO {
     private static final String INSERT_RSVP_SQL = "INSERT INTO rsvps (student_id, event_id) VALUES (?, ?)";
     private static final String DELETE_RSVP_SQL = "DELETE FROM rsvps WHERE student_id = ? AND event_id = ?";
 
-    public void addRSVP(int studentId, int eventId) {
+    public void addRSVP(RSVP rsvp) {
         try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(INSERT_RSVP_SQL)) {
-            preparedStatement.setInt(1, studentId);
-            preparedStatement.setInt(2, eventId);
+            preparedStatement.setInt(1, rsvp.getStudentId());
+            preparedStatement.setInt(2, rsvp.getEventId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void removeRSVP(int studentId, int eventId) {
+    public void removeRSVP(RSVP rsvp) {
         try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(DELETE_RSVP_SQL)) {
-            preparedStatement.setInt(1, studentId);
-            preparedStatement.setInt(2, eventId);
+            preparedStatement.setInt(1, rsvp.getStudentId());
+            preparedStatement.setInt(2, rsvp.getEventId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
---
+++ src/main/webapp/cancelRSVP.jsp
@@ -0,0 +1,6 @@
+<form action="cancelRSVP" method="post">
+    <input type="hidden" name="studentId" value="${param.studentId}">
+    <input type="hidden" name="eventId" value="${param.eventId}">
+    <p>Are you sure you want to cancel your RSVP?</p>
+    <input type="submit" value="Yes, Cancel RSVP">
+</form>
