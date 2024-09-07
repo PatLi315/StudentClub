73b6fd1f84abfe867c1e967d1e56a863923364c3
PatLi315 <zeqianl1@student.unimelb.edu.au>
2024-09-07 17:53:26 +1000

    Add Student Class

---
+++ src/src/main/java/com/unimelb/swen90007/studentclub/model/Student.java
@@ -0,0 +1,66 @@
+package com.unimelb.swen90007.studentclub.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private String password;  // This should be hashed in real-world applications
+
+    // Constructor with all fields
+    public Student(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Constructor without id (useful when creating a new student)
+    public Student(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
